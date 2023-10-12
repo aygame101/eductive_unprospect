391120c0fcbaf7cc9cfdea7c7ec8e3b60a6fabd3
Gino VALTON <42804032+aygame101@users.noreply.github.com>
2023-10-12 20:09:55 +0200

    Update hera.php

--- admin/hera.php
+++ admin/hera.php
@@ -1,8 +1,7 @@
 <?php
 // mdp admins, retirer de github .gitignore
 
-$PASSWORD_ONE = "e91f0a27f32a743d749b66d2e75ea72c36434073a3b0577e12a8a88d29b8a8a6";
+$PASSWORD_ONE = "";
 
-// TS9R$Q#8Rtju!9gRi$p6956hf@?Z%2
 // https://emn178.github.io/online-tools/sha256.html
 ?>
