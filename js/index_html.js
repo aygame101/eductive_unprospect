850b86d5a6f46d244e9c4bbab57e713b3c42b1cb
aygame101 <aygame101@gmail.com>
2023-10-13 08:40:40 +0200

    Ajout modif javascript pour modif dynamique

--- js/index_html.js
+++ js/index_html.js
@@ -71,13 +71,13 @@
 function updateChamp() {
     var champ1 = document.getElementById("ecole_id");
     var champ2 = document.getElementById("specialite_id");
+    var annee = document.getElementById("annee_id");
     
     // Supprimer toutes les options existantes du champ2
     champ2.innerHTML = "";
     
     // Ajouter les nouvelles options en fonction de la sélection du champ1
     if (champ1.value === "PPA") {
-
       var opt1_1 = new Option("Management Commercial", "Management Commercial");
       var opt1_2 = new Option("Marketing", "Marketing");
       var opt1_3 = new Option("Ressources Humaines", "Ressources Humaines");
@@ -90,10 +90,20 @@ function updateChamp() {
       champ2.add(opt1_4);
       champ2.add(opt1_6);
       champ2.add(opt1_7);
+      var opt1_10 = new Option("Bac +1", "Bac +1");
+      var opt1_11 = new Option("Bac +2", "Bac +2");
+      var opt1_12 = new Option("Bac +3", "Bac +3");
+      var opt1_13 = new Option("Bac +4", "Bac +4");
+      annee.add(opt1_10);
+      annee.add(opt1_11);
+      annee.add(opt1_12);
+      annee.add(opt1_13);
+
+
     } else if (champ1.value === "Esupcom") {
-      var opt2_1 = new Option("Communication global (bac+1 et bac+2)", "Communication global (bac+1 et bac+2)");
-      var opt2_2 = new Option("Bachelor Communication des Marques et des Entreprises (bac+3)", "Bachelor Communication des Marques et des Entreprises (bac+3)");
-      var opt2_3 = new Option("Bachelor Communication digitale et publicité (bac+3)", "Bachelor Communication digitale et publicité (bac+3)");
+      var opt2_1 = new Option("Bachelor Tronc commun (Bac+1 à +2)", "Bachelor Tronc commun (Bac+1 à +2)");
+      var opt2_2 = new Option("Bachelor Communication des entreprises et des marques (Bac+3)", "Bachelor Communication des entreprises et des marques (Bac+3)");
+      var opt2_3 = new Option("Bachelor Communication digitale et publicité (Bac+3)", "Bachelor Communication digitale et publicité (Bac+3)");
       var opt2_4 = new Option("Bachelor Evenementiel et Relations publiques (bac+3)", "Bachelor Evenementiel et Relations publiques (bac+3");
       var opt2_5 = new Option("Mastère Communication des Marques et des Entreprises (bac+4 à bac+5)", "Mastère Communication des Marques et des Entreprises (bac+4 à bac+5)");
       var opt2_6 = new Option("Mastère Communication Digitale et Publicité (bac+4 à bac+5)", "Mastère Communication Digitale et Publicité (bac+4 à bac+5)");
@@ -105,7 +115,7 @@ function updateChamp() {
       champ2.add(opt2_5);
       champ2.add(opt2_6);
       champ2.add(opt2_7);
-    } else if (champ1.value === "efab") {
+    } else if (champ1.value === "EFAB") {
       var opt3_1 = new Option("Bachelor Immobilier (bac+1 à bac+2)", "Bachelor Immobilier (bac+1 à bac+2)");
       var opt3_2 = new Option("BTS Professions Immobilieres (bac+1 à bac+2)", "BTS Professions Immobilieres (bac+1 à bac+2)");
       var opt3_3 = new Option("Bachelor Gestionnaire et Parc Immobilier (bac+3)", "Bachelor Gestionnaire et Parc Immobilier (bac+3)");
@@ -121,7 +131,7 @@ function updateChamp() {
       champ2.add(opt3_6);
       champ2.add(opt3_7);
     }
-    else if (champ1.value === "PPA_sport") {
+    else if (champ1.value === "PPA Sport") {
       var opt4_1 = new Option("Général", "Général");
       champ2.add(opt4_1);
     } else if (champ1.value === "ESGI") {
@@ -135,7 +145,7 @@ function updateChamp() {
       champ2.add(opt5_3);
       champ2.add(opt5_4);
       champ2.add(opt5_5);
-    } else if (champ1.value === "Maestris_bts") {
+    } else if (champ1.value === "Maestris BTS") {
       var opt6_1 = new Option("BTS Communication", "BTS Communication");
       var opt6_2 = new Option("BTS Management commercial opérationnel", "BTS Management commercial opérationnel");
       var opt6_3 = new Option("BTS Négociation et digitalisation de la relation client", "BTS Négociation et digitalisation de la relation client");
@@ -150,7 +160,7 @@ function updateChamp() {
       champ2.add(opt6_5);
       champ2.add(opt6_6);
       champ2.add(opt6_7);
-    } else if (champ1.value === "Efet_studio_crea") {
+    } else if (champ1.value === "Efet Studio Créa") {
       var opt7_1 = new Option("Général", "Général");
       champ2.add(opt7_1);
     }
